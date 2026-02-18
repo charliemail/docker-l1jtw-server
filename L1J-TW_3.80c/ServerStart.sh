6d6253e5844e8afbae7139fd6d42000ab8a45ba7
CillianHsueh <s3360053@gmail.com>
2026-02-18 22:03:01 +0800

    fixed: 解決 Java 執行會有路徑取不到問題

--- L1J-TW_3.80c/ServerStart.sh
+++ L1J-TW_3.80c/ServerStart.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-# Program:
-# 	The program will show it's name and first 3 parameters.
-# History:
-# 2005/08/25	VBird	First release
-PATH=.:/usr/local/bin:/usr/bin:/bin:/usr/games:/usr/local/jdk1.7.0/bin
-export PATH
+set -e
 
-if [ $(echo "show databases;" | mysql -h $DB_HOST -u root -p$DB_PWD | grep "l1jdb380") = "l1jdb380" ]; then
+# 不要覆蓋 PATH（你原本那行會把 Java 路徑踢掉，導致 java not found）
+export PATH=".:/usr/local/bin:/usr/bin:/bin:/usr/games:${PATH}"
+
+: "${DB_HOST:?DB_HOST is required}"
+: "${DB_PWD:?DB_PWD is required}"
+
+# 1) 關掉 client 端 SSL 要求（避免 ERROR 2026）
+# MYSQL=(mysql --ssl-mode=DISABLED -h "${DB_HOST}" -u root -p"${DB_PWD}")
+MYSQL=(/usr/bin/mariadb --ssl=0 -h "${DB_HOST}" -u root -p"${DB_PWD}")
+
+# 2) 修 line 9：用 grep -q 避免空字串造成 unary operator expected
+if echo "SHOW DATABASES;" | "${MYSQL[@]}" | grep -q "^l1jdb380$"; then
   echo "Database l1jdb380 exist!"
 else
-  echo "create database l1jdb380;" | mysql -h $DB_HOST -u root -p$DB_PWD
-  mysql -h $DB_HOST -u root -p$DB_PWD l1jdb380 < /data/db/l1jdb_Taiwan.sql
+  echo "create database l1jdb380;" | "${MYSQL[@]}"
+  "${MYSQL[@]}" l1jdb380 < /data/db/l1jdb_Taiwan.sql
 fi
 
 cd /data/config/
 
-# DB Related
 sed 's/mysql:\/\/localhost/mysql:\/\/'"$DB_HOST"'/g' server.properties > custom.server; cat custom.server > server.properties
 sed 's/Password=password/Password='"$DB_PWD"'/g' server.properties > custom.server; cat custom.server > server.properties
 
-# Exp Rate
 sed 's/RateXp = 1.0/RateXp = '"$RATE_XP"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateLawful = 1.0/RateLawful = '"$RATE_LAWFUL"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateKarma = 1.0/RateKarma = '"$RATE_KARMA"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateDropAdena = 1.0/RateDropAdena = '"$RATE_DROP_GOLD"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateDropItems = 1.0/RateDropItems = '"$RATE_DROP_ITEM"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 
-# Enchant Chance
 sed 's/EnchantChanceWeapon = 0/EnchantChanceWeapon = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/EnchantChanceArmor = 0/EnchantChanceArmor = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/AttrEnchantChance = 10/AttrEnchantChance = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
